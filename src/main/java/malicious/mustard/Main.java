919edf0cd81a80187a9470bad1fc903efbbf8bc5
Alexandros Ntousias <antousias@gmail.com>
2016-02-28 20:59:27 +0000

    Added Jetty

--- src/main/java/malicious/mustard/Main.java
+++ src/main/java/malicious/mustard/Main.java
@@ -1,19 +1,32 @@
 package malicious.mustard;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceFilter;
 
 public class Main {
     
-    public void sayHello() {
-        System.out.println("Hello world!");
+    private static final int DEFAULT_PORT = 8080;
+    
+    public void startServer(int serverPort) throws Exception {
+        Server server = new Server(serverPort);
+        ServletContextHandler servletContextHandler = new ServletContextHandler(server, "/");
+        servletContextHandler.addFilter(GuiceFilter.class, "/*", null);
+        servletContextHandler.addServlet(DefaultServlet.class, "/");
+        
+        server.start();
+        server.join();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Injector injector = Guice.createInjector();
         
         Main main = injector.getInstance(Main.class);
-        main.sayHello();
+        main.startServer(DEFAULT_PORT);
     }
     
 }
